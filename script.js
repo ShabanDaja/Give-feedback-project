4917f27b283b6f2f4a61a12aed8e0d96bba0ce5d
Shaban Daja <shabandaja710@gmail.com>
2023-09-28 14:31:08 +0200

    Refactoring

--- script.js
+++ script.js
@@ -1,119 +1,166 @@
+// -- GLOBAL --
+const MAX_CHARS = 150;
+const BASE_API_URL = 'https://bytegrad.com/course-assets/js/1/api';
+
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
 const feedbackListEl = document.querySelector('.feedbacks');
 const submitBtnEl = document.querySelector('.submit-btn');
-const MAX_CHARS = 150;
 const spinnerEl = document.querySelector('.spinner');
 
-// COUNTER
+const renderFeedbackItem = feedbackItem => {
+    // new feedback item HTML
+    const feedbackItemHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${feedbackItem.company}</p>
+                <p class="feedback__text">${feedbackItem.text}</p>
+            </div>
+            <p class="feedback__date">${feedbackItem.daysAgo === 0 ? 'NEW' : `${feedbackItem.daysAgo}d`}</p>
+        </li>
+    `;
+
+    // insert new feedback item in list
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+};
+
+
+// -- COUNTER COMPONENT --
 const inputHandler = () => {
-    // max number of characters
-    const maxNrchars = MAX_CHARS;
-    
-    // number of characters currently typed
+    // determine maximum number of characters
+    const maxNrChars = MAX_CHARS;
+
+    // determine number of characters currently typed
     const nrCharsTyped = textareaEl.value.length;
-    
-    //calculate number of characters left
-    const charsLeft = maxNrchars - nrCharsTyped;
-    
-    //show number of char left
+
+    // calculate number of characters left (maximum minus currently typed)
+    const charsLeft = maxNrChars - nrCharsTyped;
+
+    // show number of characters left
     counterEl.textContent = charsLeft;
 };
 
 textareaEl.addEventListener('input', inputHandler);
 
-// SUBMIT 
-const showIndicator = textCheck => {
 
+// -- FORM COMPONENT --
+const showVisualIndicator = textCheck => {
     const className = textCheck === 'valid' ? 'form--valid' : 'form--invalid';
 
+    // show valid indicator
     formEl.classList.add(className);
-        setTimeout(() => {
-           formEl.classList.remove(className);
-        }, 2000);
-}
+
+    // remove visual indicator
+    setTimeout(() => {
+        formEl.classList.remove(className);
+    }, 2000);
+};
+
 const submitHandler = event => {
-    //prevent default browser action
+    // prevent default browser action (submitting form data to 'action'-address and refreshing page)
     event.preventDefault();
 
-    // GET TEXT from text area
+    // get text from textarea
     const text = textareaEl.value;
     
-    // Validate text (#hashtag is present and text is long enough)
-    if (text.includes('#') && text.length > 4) {
-        showIndicator('valid');
+    // validate text (e.g. check if #hashtag is present and text is long enough)
+    if (text.includes('#') && text.length >= 5) {
+        showVisualIndicator('valid');
     } else {
-        showIndicator('invalid');
+        showVisualIndicator('invalid');
 
-         textareaEl.focus();
-         
-         return;
-    
+        // focus textarea
+        textareaEl.focus();
+
+        // stop this function execution
+        return;
     }
 
+    // we have text, now extract other info from text
     const hashtag = text.split(' ').find(word => word.includes('#'));
     const company = hashtag.substring(1);
     const badgeLetter = company.substring(0, 1).toUpperCase();
     const upvoteCount = 0;
-    const daysAGO = 0;
+    const daysAgo = 0;
 
-    const feedbackItemHTML = `
-        <li class="feedback">
-            <button class="upvote">
-                <i class="fa-solid fa-caret-up upvote__icon"></i>
-                <span class="upvote__count">${upvoteCount}</span>
-            </button>
-            <section class="feedback__badge">
-                 <p class="feedback__letter">${badgeLetter}</p>
-            </section>
-            <div class="feedback__content">
-                <p class="feedback__company">${company}</p>
-                <p class="feedback__text">${text}</p>
-            </div>
-            <p class="feedback__date">${daysAGO === 0 ? 'NEW' : `${daysAgo}d` }</p>
-        </li>
-    `;
+    // render feedback item in list
+    const feedbackItem = {
+        upvoteCount: upvoteCount,
+        company: company,
+        badgeLetter: badgeLetter,
+        daysAgo: daysAgo,
+        text: text
+    };
+    renderFeedbackItem(feedbackItem);
 
-    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+    // send feedback item to server
+    fetch(`${BASE_API_URL}/feedbacks`, {
+        method: 'POST',
+        body: JSON.stringify(feedbackItem),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (!response.ok) {
+            console.log('Something went wrong');
+            return;
+        }
+        
+        console.log('Successfully submitted');
+    }).catch(error => console.log(error));
 
-    // Clear text area after i click submit and blur it
+    // clear textarea
     textareaEl.value = '';
+
+    // blur submit button
     submitBtnEl.blur();
+
+    // reset counter
     counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener('submit', submitHandler);
 
-// FEEDBACK LIST
 
-fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+// -- FEEDBACK LIST COMPONENT --
+const clickHandler = event => {
+    const clickedEl = event.target;
+    const upvoteIntention = clickedEl.className.includes('upvote');
+    if (upvoteIntention) {
+        const upvoteBtnEl = clickedEl.closest('.upvote');
+        upvoteBtnEl.disabled = true;
+        const upvoteCountEl = upvoteBtnEl.querySelector('.upvote__count');
+        let upvoteCount = +upvoteCountEl.textContent;
+        upvoteCountEl.textContent = ++upvoteCount;
+    } else {
+        clickedEl.closest('.feedback').classList.toggle('feedback--expand');
+    }
+
+    
+};
+
+feedbackListEl.addEventListener('click', clickHandler);
+
+fetch(`${BASE_API_URL}/feedbacks`)
     .then(response => response.json())
     .then(data => {
-
-        //remove spinner
+        // remove spinner
         spinnerEl.remove();
 
-        data.feedbacks.forEach(feedbackItem => {
-            
-            const feedbackItemHTML = `
-                <li class="feedback">
-                    <button class="upvote">
-                        <i class="fa-solid fa-caret-up upvote__icon"></i>
-                        <span class="upvote__count">${feedbackItem.upvoteCount}</span>
-                    </button>
-                    <section class="feedback__badge">
-                        <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
-                    </section>
-                    <div class="feedback__content">
-                        <p class="feedback__company">${feedbackItem.company}</p>
-                        <p class="feedback__text">${feedbackItem.text}</p>
-                    </div>
-                    <p class="feedback__date">${feedbackItem.daysAGO === 0 ? 'NEW' : `${feedbackItem.daysAgo}d` }</p>
-                </li>
-            `;
-    
-            feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
-        });
-
+        // iterate over each element in feedbacks array and render it in list
+        data.feedbacks.forEach(feedbackItem => renderFeedbackItem(feedbackItem));
+    })
+    .catch(error => {
+        feedbackListEl.textContent = `Failed to fetch feedback items. Error message: ${error.message}`;
     });
+
+    
