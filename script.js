90faca333b1174dfca86ab8fc10a656f46d875fc
Shaban Daja <shabandaja710@gmail.com>
2023-09-28 10:09:15 +0200

    Validate text when i press SUBMIT

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
+const formEl = document.querySelector('.form');
 
+
+// COUNTER
 const inputHandler = () => {
     // max number of characters
     const maxNrchars = 150;
@@ -16,3 +19,36 @@ const inputHandler = () => {
 };
 
 textareaEl.addEventListener('input', inputHandler);
+
+// SUBMIT 
+const submitHandler = event => {
+    //prevent default browser action
+    event.preventDefault();
+
+    // GET TEXT from text area
+    const text = textareaEl.value;
+    
+    // Validate text (#hashtag is present and text is long enough)
+    if (text.includes('#') && text.length > 4) {
+        formEl.classList.add('form--valid');
+        setTimeout(() => {
+           formEl.classList.remove('form--valid') 
+        }, 2000);
+    } else {
+        formEl.classList.add('form--invalid');
+        setTimeout(() => {
+            formEl.classList.remove('form--invalid') 
+         }, 2000);
+
+         textareaEl.focus();
+         
+         return;
+    
+    }
+
+    
+    
+
+};
+
+formEl.addEventListener('submit', submitHandler);
