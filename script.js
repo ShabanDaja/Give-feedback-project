a1d5d2e9763adc0fc08fe37b1bac246b4dd76fc1
Shaban Daja <shabandaja710@gmail.com>
2023-09-28 10:56:03 +0200

    When i press submit, the feedback shows.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const textareaEl = document.querySelector('.form__textarea');
 const counterEl = document.querySelector('.counter');
 const formEl = document.querySelector('.form');
+const feedbackListEl = document.querySelector('.feedbacks');
+const submitBtnEl = document.querySelector('.submit-btn');
 
 
 // COUNTER
@@ -46,9 +48,35 @@ const submitHandler = event => {
     
     }
 
-    
-    
+    const hashtag = text.split(' ').find(word => word.includes('#'));
+    const company = hashtag.substring(1);
+    const badgeLetter = company.substring(0, 1).toUpperCase();
+    const upvoteCount = 0;
+    const daysAGO = 0;
 
+    const feedbackItemHTML = `
+        <li class="feedback">
+            <button class="upvote">
+                <i class="fa-solid fa-caret-up upvote__icon"></i>
+                <span class="upvote__count">${upvoteCount}</span>
+            </button>
+            <section class="feedback__badge">
+                 <p class="feedback__letter">${badgeLetter}</p>
+            </section>
+            <div class="feedback__content">
+                <p class="feedback__company">${company}</p>
+                <p class="feedback__text">${text}</p>
+            </div>
+            <p class="feedback__date">${daysAGO === 0 ? 'NEW' : `${daysAgo}d` }</p>
+        </li>
+    `;
+
+    feedbackListEl.insertAdjacentHTML('beforeend', feedbackItemHTML);
+
+    // Clear text area after i click submit and blur it
+    textareaEl.value = '';
+    submitBtnEl.blur();
+    counterEl.textContent = '150';
 };
 
 formEl.addEventListener('submit', submitHandler);
